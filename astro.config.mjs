ad5df7802cce899b0e7b7c83dbd173568630dc20
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-12 22:12:34 +0800

    cloudflare from astrojs please work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  adapter: node({
-      mode: 'standalone'
-  }),
-
+  adapter: cloudflare(),
   integrations: [svelte()]
 });
