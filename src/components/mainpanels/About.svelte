6a1541a7b16c7d9373e86f210c4587352e17f766
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-05-17 10:42:35 +0800

    make groups for skills

--- src/components/mainpanels/About.svelte
+++ src/components/mainpanels/About.svelte
@@ -3,23 +3,53 @@
     import LastFM from '../misc/About/LastFM.svelte';
     import WakatimeStats from '../misc/About/WakatimeStats.svelte';
 
-    const skills = [
-        { name: "Lua", icon: "https://skillicons.dev/icons?i=lua" },
-        { name: "Haxe", icon: "https://skillicons.dev/icons?i=haxe" },
-        { name: "Python", icon: "https://skillicons.dev/icons?i=python" },
-        { name: "HTML", icon: "https://skillicons.dev/icons?i=html" },
-        { name: "CSS", icon: "https://skillicons.dev/icons?i=css" },
-        { name: "Rust", icon: "https://skillicons.dev/icons?i=rust" },
-        { name: "Go", icon: "https://skillicons.dev/icons?i=go" },
-        { name: "JavaScript", icon: "https://skillicons.dev/icons?i=js" },
-        { name: "TypeScript", icon: "https://skillicons.dev/icons?i=ts" },
-        { name: "Bash", icon: "https://skillicons.dev/icons?i=bash" },
-        { name: "Svelte", icon: "https://skillicons.dev/icons?i=svelte" },
-        { name: "SolidJS", icon: "https://skillicons.dev/icons?i=solidjs" },
-        { name: "Astro", icon: "https://skillicons.dev/icons?i=astro" },
-        { name: "React", icon: "https://skillicons.dev/icons?i=react" },
-        { name: "Express", icon: "https://skillicons.dev/icons?i=express" }
+    const skillGroup = [
+        { group: "💻 Languages", skills: [
+            { name: "Lua", icon: "lua", state: "yellow" },
+            { name: "Haxe", icon: "haxe", state: "yellow" },
+            { name: "Python", icon: "python", state: "blue" },
+            { name: "HTML", icon: "html", state: "blue" },
+            { name: "CSS", icon: "css", state: "blue" },
+            { name: "Rust", icon: "rust", state: "red" },
+            { name: "Go", icon: "go", state: "red" },
+            { name: "JavaScript", icon: "js", state: "blue" },
+            { name: "TypeScript", icon: "ts", state: "yellow" },
+            { name: "Bash", icon: "bash", state: "yellow" },
+        ]},
+
+        { group: "📦 Frontend Frameworks", skills: [
+            { name: "Svelte", icon: "svelte", state: "blue" },
+            { name: "SolidJS", icon: "solidjs", state: "yellow" },
+            { name: "Astro", icon: "astro", state: "blue" },
+            { name: "React", icon: "react", state: "yellow" },
+        ]},
+
+        { group: "🖥️ Backend & Tools", skills: [
+            { name: "Express", icon: "express", state: "blue" },
+            { name: "Node.js", icon: "nodejs", state: "blue" },
+            { name: "Flask", icon: "flask", state: "yellow" },
+            { name: "Tauri", icon: "tauri", state: "red" },
+            { name: "PyWebView", icon: "https://pywebview.flowrl.com/logo-no-text.png", state: "blue" }
+        ]}
     ];
+
+    function isExternalIcon(icon) {
+        if (!icon.includes("https")) return `https://skillicons.dev/icons?i=${icon}`;
+        return icon;
+    }
+
+    function getIconState(icon) {
+        switch (icon) {
+            case "blue":
+                return "#008BFF";
+            case "yellow":
+                return "#FFFF00";
+            case "red":
+                return "#FF0000";
+            default:
+                return "#008BFF";
+        }
+    }
 </script>
 
 <main>
@@ -36,22 +66,33 @@
         <section>
             <span>
                 I am a self-taught teenager open source software developer. I make websites, software, and tools! <br/>
-                I'm also an artist! I create designs, make furry cartoony arts and practicing! <br/>  
+                I'm also an artist! I create designs, make furry cartoony arts and practicing! <br/>
             </span>
             
             <span class="skills">
-                <span class="bigText">💻 Skills/Tech Stack</span> <br/>
+                <span class="bigText">💻 Skills/Tech Stack</span>
+                <span class="smallText">Tap/Hover Information:</span>
+                <span style="color: #008BFF;"><b>Blue</b> - Prefers this language, and codes in it</span> <br>
+                <span style="color: #FFFF00;"><b>Yellow</b> - Codes this languages, but doesn't mainly code in it.</span> <br>
+                <span style="color: #FF0000;"><b>Red</b> - Learning on this language.</span>
+                
                 <section class="skillContainer">
-                    {#each skills as skill}
-                        <div class="skillItem">
-                            <img
-                                src={skill.icon}
-                                alt={skill.name}
-                                style="width: 3rem"
-                            />
-                            <span class="name">
-                                {skill.name}
-                            </span>
+                    {#each skillGroup as skill}
+                        <span class="bigText" style="font-size: 1.5rem">{skill.group}</span>
+                        <div class="skillGroup">
+                            {#each skill.skills as skill}
+                                <div class="skillItem" style="--hoverColor: {getIconState(skill.state)};">
+                                    <img
+                                        src={isExternalIcon(skill.icon)}
+                                        alt={skill.name}
+                                        style="width: 3rem"
+                                    />
+                                    
+                                    <span class="name">
+                                        {skill.name}
+                                    </span>
+                                </div>
+                            {/each}
                         </div>
                     {/each}
                 </section>
@@ -94,28 +135,36 @@
             .skills {
                 .skillContainer {
                     display: flex;
-                    flex-direction: row;
+                    flex-direction: column;
                     flex-wrap: wrap;
                     gap: 10px;
+                    margin-top: 10px;
 
-                    .skillItem {
+                    .skillGroup {
                         display: flex;
-                        align-items: center;
+                        flex-direction: row;
+                        flex-wrap: wrap;
                         gap: 10px;
-                        background-color: var(--sidebar);
-                        border: 2px solid var(--border);
-                        padding: 5px 10px;
-                        border-radius: 10px;
-                        width: fit-content;
-                        transition: all 0.1s ease-in-out;
-                        
-                        .name {
-                            font-size: 1.5rem;
-                        }
 
-                        &:hover {
-                            scale: 1.05;
-                            border-color: var(--primary);
+                        .skillItem {
+                            display: flex;
+                            align-items: center;
+                            gap: 10px;
+                            background-color: var(--sidebar);
+                            border: 2px solid var(--border);
+                            padding: 5px 10px;
+                            border-radius: 10px;
+                            width: fit-content;
+                            transition: all 0.1s ease-in-out;
+                            
+                            .name {
+                                font-size: 1.5rem;
+                            }
+    
+                            &:hover {
+                                scale: 1.05;
+                                border-color: var(--hoverColor);
+                            }
                         }
                     }
                 }
@@ -138,5 +187,8 @@
         display: flex;
         font-size: 2rem;
         font-weight: bold;
+    } .smallText {
+        display: flex;
+        font-size: 1.25rem;
     }
 </style>
